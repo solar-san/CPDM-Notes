49604301124e07f267cdddc84d11a5baf2f805a5
solar-san <solar-san@proton.me>
2023-10-13 17:29:31 +0200

    Render HTML + overall commit
    - Fixed grammar
    - Converted to qmd
    - Fixed broken code cells

---
+++ Period 2/my_first_module.py
@@ -0,0 +1,14 @@
+### Functions:
+
+
+def type_print(obj):
+    '''
+    IN: any python object.
+    OUT: pretty prints the objects, followed by its type.
+    '''
+    print(obj, '--->', type(obj))
+
+### Variables:
+
+std_encoding = "utf-8"
+file_folder_path = '/Users/themagician/Documents/DABS/Appunti/Computer_Programming_and_Data_Management/Files/'
